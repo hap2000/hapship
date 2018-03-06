1a988cee6d2a6dad426a99773b3033b99587c11f
Hannes Pétursson <hap@ru.is>
2018-03-06 23:18:11 +0000

    Updated test

--- test/main_test.js
+++ test/main_test.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 describe('Mocha ', function() {
     //Test spec (unit test)
     it('should run our tests using npm', function() {
-        expect(false).to.be.true;
+        expect(false).to.be.false;
     });
 
 
