b281187ee672faca69d55a2b94beaf4e553008a2
Hannes Pétursson <hap@ru.is>
2018-03-06 23:17:09 +0000

    Added changes

--- test/main_test.js
+++ test/main_test.js
@@ -1,9 +1,10 @@
 var expect = require('chai').expect;
 
+//Test suite
 describe('Mocha ', function() {
     //Test spec (unit test)
     it('should run our tests using npm', function() {
-        expect(true).to.be.ok;
+        expect(false).to.be.true;
     });
 
 
